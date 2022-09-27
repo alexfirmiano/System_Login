86f67176b9042efceb819051bee9efc402d4897e
AlexBF_Pro <alexfirmiano2@gmail.com>
2022-09-27 10:49:24 -0300

    Codificação page home, styles e correções

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/Index";
-import Signin from "../pages/Signin/Index";
-import Signup from "../pages/Signup/Index";
+import Home from "../pages/Home/index";
+import Signin from "../pages/Signin/index";
+import Signup from "../pages/Signup/index";
 import useAuth from "../hooks/useAuth";
 
 const Private = ({ Item }) => {
